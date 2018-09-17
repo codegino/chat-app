c4896068591e5f506f32d5869f2fff12fdd174bb
Carlo Gino Catapang <carloginocatapang@gmail.com>
2018-09-17 14:38:36 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const express = require('express');
 
+const port = process.env.PORT || 3000;
+
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
 
 app.use(express.static(publicPath));
 
-console.log(__dirname + '/../public');
-console.log(publicPath);
-
-app.listen(3000, () => {
-  console.log('App starting on port 3000')
+app.listen(port, () => {
+  console.log(`App starting on port ${port}`)
 });
