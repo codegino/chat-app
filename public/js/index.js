7267e480d37684538e6ea386dcc7c4e7a80a3775
Carlo Gino Catapang <carloginocatapang@gmail.com>
2018-09-17 16:04:05 +0800

    Add files via upload

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+const socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to Server');
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from Server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('New message', message);
+});
--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected!')
+
+  socket.on('createMessage', (message) => {
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`App starting on port ${port}`)
 });
